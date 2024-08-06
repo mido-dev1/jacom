d2464aaafff53d663519d2ff7fe158ffdfc4577f
mido-dev1 <123629444+mido-dev1@users.noreply.github.com>
2024-08-07 00:34:15 +0100

    Setting up AST

--- lox/Lox.java
+++ lox/Lox.java
@@ -9,67 +9,67 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-    static boolean hadError = false;
+  static boolean hadError = false;
 
-    public static void main(String[] args) throws IOException {
-        if (args.length > 1) {
-            System.out.println("Usage: jlox [script]");
-            System.exit(64);
-        } else if (args.length == 1) {
-            runFile(args[0]);
-        } else {
-            runPrompt();
-        }
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
     }
+  }
 
-    // file
-    private static void runFile(String path) throws IOException {
-        byte[] bytes = Files.readAllBytes(Paths.get(path));
-        run(new String(bytes, Charset.defaultCharset()));
+  // file
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
 
-        // Indicate an error in the exit code.
-        if (hadError)
-            System.exit(65);
+    // Indicate an error in the exit code.
+    if (hadError)
+      System.exit(65);
+  }
+
+  // prompt
+  private static void runPrompt() throws IOException {
+    // Read from stdin
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null)
+        break;
+      run(line);
+      hadError = false;
     }
+  }
 
-    // prompt
-    private static void runPrompt() throws IOException {
-        // Read from stdin
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
+  // The run
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
 
-        for (;;) {
-            System.out.print("> ");
-            String line = reader.readLine();
-            if (line == null)
-                break;
-            run(line);
-            hadError = false;
-        }
+    // For now, just print the tokens.
+    for (Token token : tokens) {
+      System.out.println(token);
     }
+  }
 
-    // The run
-    private static void run(String source) {
-        Scanner scanner = new Scanner(source);
-        List<Token> tokens = scanner.scanTokens();
+  // error handling
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
 
-        // For now, just print the tokens.
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
-    }
-
-    // error handling
-    static void error(int line, String message) {
-        report(line, "", message);
-    }
-
-    private static void report(int line, String where,
-            String message) {
-        System.err.println(
-                "[line " + line + "] Error" + where + ": " + message);
-        hadError = true;
-    }
+  private static void report(int line, String where,
+      String message) {
+    System.err.println(
+        "[line " + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
 }
 
 // example: var language = "lox";
--- lox/Token.java
+++ lox/Token.java
@@ -1,20 +1,19 @@
 package lox;
 
 class Token {
-	final TokenType type;
-	final String lexeme;
-	final Object literal;
-	final int line;
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line;
 
-	Token(TokenType type, String lexeme, Object literal, int line) {
-		this.type = type;
-		this.lexeme = lexeme;
-		this.literal = literal;
-		this.line = line;
-	}
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
 
-	public String toString() {
-		return type + " " + lexeme + " " + literal;
-		// return type + " " + lexeme + " " + literal + " " + line;
-	}
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
 }
